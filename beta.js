8a441b8a98fc6523cf5d7fc7bdcab052e84fd2ca
adamj200 <adamj200@hotmail.com>
2018-09-28 16:00:02 +0100

    Update beta.js

--- beta.js
+++ beta.js
@@ -1,9 +1,23 @@
 if (receivedMessage.channel.id == xxxxxxxxxxxxxxx) {
   if (receivedMessage.author != client.user) {
     if (receivedMessage.content.includes(client.user.toString())) {
-      var userMessage = receivedMessage.content.toLowerCase().replace(client.user.toString(),"");
+      var region
+      var userMessage = receivedMessage.content.toLowerCase().replace(client.user.toString(),"")
       if (userMessage.substring(0, 3) == "add") {
         var regionAccount = userMessage.substring(4);
+          if (totalWords.replace(/ .*/,'').toLowerCase() == "euw") {
+            region = "euw
+          }
+          else if (totalWords.replace(/ .*/,'').toLowerCase() == "na") {
+            region = "na"
+          }
+          else if (totalWords.replace(/ .*/,'').toLowerCase() == "eune") {
+            region = "eune"
+          }
+        
+        
+        
+        
         //if first word = region, store and split string
       }
     }

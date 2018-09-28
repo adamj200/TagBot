0a21dc4b3a1be4d6c2572260f92835c4c7dc8f4c
adamj200 <adamj200@hotmail.com>
2018-09-28 12:01:32 +0100

    Update beta.js

--- beta.js
+++ beta.js
@@ -1,8 +1,10 @@
 if (receivedMessage.channel.id == xxxxxxxxxxxxxxx) {
   if (receivedMessage.author != client.user) {
     if (receivedMessage.content.includes(client.user.toString())) {
-      if (receivedMessage.content.toLowerCase().    ("add") { //need to test full string including @
-        
+      var userMessage = receivedMessage.content.toLowerCase().replace(client.user.toString(),"");
+      if (userMessage.substring(0, 3) == "add") {
+        var regionAccount = userMessage.replace("add ","");
+        //if first word = region, store and split string
       }
     }
   }

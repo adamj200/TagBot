f99d788d30fd689d80d439dd9919671f42c20bda
adamj200 <adamj200@hotmail.com>
2018-09-28 11:48:54 +0100

    Create beta.js

---
+++ beta.js
@@ -0,0 +1,13 @@
+if (receivedMessage.channel.id == xxxxxxxxxxxxxxx) {
+  if (receivedMessage.author != client.user) {
+    if (receivedMessage.content.includes(client.user.toString())) {
+      if (receivedMessage.content.toLowerCase().    ("add") { //need to test full string including @
+        
+      }
+    }
+  }
+}
+
+
+
+//@TagBot add [region] [account]

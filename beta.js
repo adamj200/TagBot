b8f1c6deced7569b320a43cde4cf9d037c8f35c0
adamj200 <adamj200@hotmail.com>
2018-10-03 15:16:31 +0100

    Update beta.js

--- beta.js
+++ beta.js
@@ -1,25 +1,36 @@
+const fs = require("fs")
+
 if (receivedMessage.channel.id == xxxxxxxxxxxxxxx) {
   if (receivedMessage.author != client.user) {
     if (receivedMessage.content.includes(client.user.toString())) {
       var region
+      var account
       var userMessage = receivedMessage.content.toLowerCase().replace(client.user.toString(),"")
       if (userMessage.substring(0, 3) == "add") {
-        var regionAccount = userMessage.substring(4);
-          if (totalWords.replace(/ .*/,'').toLowerCase() == "euw") {
+        var regionAccount = userMessage.substring(4)
+          if (regionAccount.replace(/ .*/,'').toLowerCase() == "euw") {
             region = "euw"
+            account = regionAccount.substring(4)
           }
-          else if (totalWords.replace(/ .*/,'').toLowerCase() == "na") {
+          else if (regionAccount.replace(/ .*/,'').toLowerCase() == "na") {
             region = "na"
+            account = regionAccount.substring(3)
           }
-          else if (totalWords.replace(/ .*/,'').toLowerCase() == "eune") {
+          else if (regionAccount.replace(/ .*/,'').toLowerCase() == "eune") {
             region = "eune"
+            account = regionAccount.substring(5)
           }
-        
-        
-        
-        
-        //if first word = region, store and split string
       }
+      
+      let points = JSON.parse(fs.readFileSync("./points.json", "utf8"))
+      
+      fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  })
+        
+        
+        
+
     }
   }
 }

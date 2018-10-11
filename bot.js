f94734a315bbcd3a456f3925a4b8f177c889bde4
adamj200 <adamj200@hotmail.com>
2018-10-11 11:59:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,87 +36,87 @@ client.on('message', (receivedMessage) => {
 	
     if (receivedMessage.content.includes(client.user.toString())) {
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "euw" + "\\b")) != -1) {
-			region = "euw"
+			region = "EUW"
 			regionCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "na" + "\\b")) != -1) {
-			region = "na"
+			region = "NA"
 			regionCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "kr" + "\\b")) != -1 || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "korea" + "\\b")) != -1) {
-			region = "kr"
+			region = "KR"
 			regionCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "oce" + "\\b")) != -1) {
-			region = "oce"
+			region = "OCE"
 			regionCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "eune" + "\\b")) != -1) {
-			region = "eune"
+			region = "EUNE"
 			regionCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "lan" + "\\b")) != -1) {
-			region = "lan"
+			region = "LAN"
 			regionCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "pt" + "\\b")) != -1) {
-			region = "pt"
+			region = "PT"
 			regionCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "jpn" + "\\b")) != -1 || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "japan" + "\\b")) != -1) {
-			region = "jpn"
+			region = "JPN"
 			regionCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "tr" + "\\b")) != -1 || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "turkey" + "\\b")) != -1) {
-			region = "tr"
+			region = "TR"
 			regionCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "br" + "\\b")) != -1 || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "brazil" + "\\b")) != -1) {
-			region = "br"
+			region = "BR"
 			regionCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "las" + "\\b")) != -1) {
-			region = "las"
+			region = "LAS"
 			regionCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "rugged" + "\\b")) != -1) {
-			skin = "rugged"
+			skin = "Rugged"
 			skinCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "steel" + "\\b")) != -1 || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "legion" + "\\b")) != -1  || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "steellegion" + "\\b")) != -1) {
-			skin = "steel legion"
+			skin = "Steel Legion"
 			skinCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "rogue" + "\\b")) != -1 || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "admiral" + "\\b")) != -1  || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "rogueadmiral" + "\\b")) != -1) {
-			skin = "rogue admiral"
+			skin = "Rogue Admiral"
 			skinCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "sanguine" + "\\b")) != -1) {
-			skin = "sanguine"
+			skin = "Sanguine"
 			skinCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "commando" + "\\b")) != -1) {
-			skin = "commando"
+			skin = "Commando"
 			skinCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "desert" + "\\b")) != -1 || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "trooper" + "\\b")) != -1  || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "deserttrooper" + "\\b")) != -1) {
-			skin = "desert trooper"
+			skin = "Desert Trooper"
 			skinCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "warring" + "\\b")) != -1 || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "kingdoms" + "\\b")) != -1  || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "warringkingdoms" + "\\b")) != -1) {
-			skin = "warring kingdoms"
+			skin = "Warring Kingdoms"
 			skinCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "dread" + "\\b")) != -1 || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "knight" + "\\b")) != -1  || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "dreadknight" + "\\b")) != -1) {
-			skin = "dreadknight"
+			skin = "Dreadknight"
 			skinCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "noble" + "\\b")) != -1 || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "chroma" + "\\b")) != -1) {
-			skin = "noble chroma"
+			skin = "Noble Chroma"
 			skinCount += 1
 		}
 		if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "god" + "\\b")) != -1 || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "king" + "\\b")) != -1  || receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "godking" + "\\b")) != -1) {
-			skin = "god king"
+			skin = "God King"
 			skinCount += 1
 		}
     }
@@ -147,7 +147,7 @@ client.on('message', (receivedMessage) => {
 		receivedMessage.member.removeRole(receivedMessage.guild.roles.find(x => x.name === "BR").id)
 		receivedMessage.member.removeRole(receivedMessage.guild.roles.find(x => x.name === "LAS").id).then(() => {
 			setTimeout(function() {
-				receivedMessage.member.addRole(receivedMessage.guild.roles.find(x => x.name === region.toUpperCase()).id)
+				receivedMessage.member.addRole(receivedMessage.guild.roles.find(x => x.name === region).id)
 			}, 500);
 		})
 	}
@@ -163,7 +163,7 @@ client.on('message', (receivedMessage) => {
 		receivedMessage.member.removeRole(receivedMessage.guild.roles.find(x => x.name === "Noble Chroma").id)
 		receivedMessage.member.removeRole(receivedMessage.guild.roles.find(x => x.name === "God King").id).then(() => {
 			setTimeout(function() {
-				receivedMessage.member.addRole(receivedMessage.guild.roles.find(x => x.name === skin.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()})).id)
+				receivedMessage.member.addRole(receivedMessage.guild.roles.find(x => x.name === skin).id)
 			}, 500);
 		})
 	}

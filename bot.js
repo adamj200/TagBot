9369e048fdbd88925403f772f2aed9af457fd663
adamj200 <adamj200@hotmail.com>
2018-10-12 14:02:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,14 +13,6 @@ client.on("message", (receivedMessage) => {
         	return
 	}
 	
-	console.log(receivedMessage.channel.id)
-	
-	if (receivedMessage.channel.id == 368816534744858637) {
-		receivedMessage.channel.send(receivedMessage.author + " author")
-		receivedMessage.channel.send(receivedMessage.member + " member")
-		return
-	}
-	
 	if (receivedMessage.channel.id != 268510171125252096) {
 		return
 	}

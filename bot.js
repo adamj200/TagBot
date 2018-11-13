02b47ea2cf175077f777e8500350048eb2677096
adamj200 <adamj200@hotmail.com>
2018-11-13 11:52:17 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,11 +38,11 @@ client.on("message", (receivedMessage) => {
 		}
 	}
 	
-	if (receivedMessage.channel.id != 511868913483776012) {
+	if (receivedMessage.channel.id == 511868913483776012) {
 		if (receivedMessage.content.includes(client.user.toString())) {
 			if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "rank" + "\\b")) != -1) {
 			{
-				receivedMessage.channel.send("receivedMessage.channel")
+				receivedMessage.channel.send(receivedMessage.channel)
 			}
 		}
 		return

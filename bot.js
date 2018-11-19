998bcd1c44afbd8087ce9c8da6c9262a1e3a5c99
adamj200 <adamj200@hotmail.com>
2018-11-19 20:19:10 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,6 +43,7 @@ client.on("message", (receivedMessage) => {
 			var userMessage = receivedMessage.content.toLowerCase().replace(client.user.toString(),"")
 			console.log(userMessage)
 			if (userMessage.toLowerCase().search(new RegExp("\\b" + "!demaciate" + "\\b")) != -1) {
+				console.log(receivedMessage.author + " Nuked")
 				receivedMessage.channel.send(receivedMessage.author + " Nuked")
 			}
 		}

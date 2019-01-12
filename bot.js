99f64413bc35e1327da56b2eed1e0ad4e788a52c
adamj200 <adamj200@hotmail.com>
2019-01-12 03:23:14 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -214,7 +214,7 @@ client.on("message", (receivedMessage) => {
 			receivedMessage.channel.send(receivedMessage.author + " Tagged")
 		}
 		else {
-			receivedMessage.react('🤔')
+			receivedMessage.react(':thinking:')
 		}
 	}			
 })

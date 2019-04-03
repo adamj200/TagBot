288d793bcfd7524c4eca6c2c20eaee64d4591b96
adamj200 <adamj200@hotmail.com>
2019-04-03 21:19:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on("message", (receivedMessage) => {
 	
 	console.log(receivedMessage.author + ": " + receivedMessage.content)
 	
-	if (receivedMessage.author == "<@198893129149972480>" || receivedMessage.author == "<@249633375407702017>" || receivedMessage.author == "<@348142087805468693>") {
+	if (receivedMessage.author == "<@198893129149972480>" || receivedMessage.author == "<@249633375407702017>" || receivedMessage.author == "<@348142087805468693>" || receivedMessage.author == "<@182727259054931969>" || receivedMessage.author == "<@243931167823233026>") {
 		if (receivedMessage.content.includes(client.user.toString())) {
 			if (receivedMessage.content.toLowerCase().search(new RegExp("\\b" + "daddy" + "\\b")) != -1) {
 				receivedMessage.member.addRole(receivedMessage.guild.roles.find(x => x.name === "Rugged").id)

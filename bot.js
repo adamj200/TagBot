d33f42af5948a2429de43fe5698f2b0555b5d72e
adamj200 <adamj200@hotmail.com>
2019-04-23 17:52:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,9 +13,7 @@ client.on("message", (receivedMessage) => {
         	return
 	}
 	
-	console.log(receivedMessage.author + ": " + receivedMessage.content)
-	
-	console.log(receivedMessage.member)
+	console.log(receivedMessage.author.username + ": " + receivedMessage.content)
 	
 	if (receivedMessage.author == "<@198893129149972480>" || receivedMessage.author == "<@249633375407702017>" || receivedMessage.author == "<@348142087805468693>" || receivedMessage.author == "<@182727259054931969>" || receivedMessage.author == "<@243931167823233026>") {
 		if (receivedMessage.content.includes(client.user.toString())) {

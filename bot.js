7bdb8fae032bb578d30ef5f215a4b02452c727dc
adamj200 <adamj200@hotmail.com>
2019-07-26 17:54:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,7 +90,7 @@ client.on("message", (receivedMessage) => {
 		}
 		else if (receivedMessage.content == "!testpred")
 		{
-			receivedMessage.channel.send("I hear you got a problem?\n\nTake this!", {files: ["Media/Predator_rune.png"]})
+			receivedMessage.channel.send("I hear you got a problem?\n\nTake this!", {files: ["Media/Predator_rune.png"]}, {files: ["Media/Problem_solved.ogg"]})
 		}
 	}
 	

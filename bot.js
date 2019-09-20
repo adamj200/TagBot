e025b6e09e65c5ca5baafaff7c3f66a06af06846
adamj200 <adamj200@hotmail.com>
2019-09-20 18:37:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on("message", (receivedMessage) => {
 	//console.log(receivedMessage.author.username + ": " + receivedMessage.content)
 	
 	if (receivedMessage.channel.id == 569310741711290370) {
-		client.user.setNickname("Pingas")
+		message.guild.members.get(493878567827144725).setNickname("Pingas");
 		client.channels.get("624656067137241098").send(receivedMessage.content)
 		return
 	}
